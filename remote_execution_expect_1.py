80c764204c93705e11a768e945a851fc58bb8217
valay12 <57678557+valay12@users.noreply.github.com>
2021-05-28 17:00:31 +0530

    Update remote_execution_expect_1.py

--- remote_execution_expect_1.py
+++ remote_execution_expect_1.py
@@ -21,18 +21,34 @@ Created on Mon Feb 25 16:24:52 2019
 
 }
 '''
-#import queue
-#from  run_single_device import run_single_device
-#import psycopg2
-#import dbconstants
-#import re
-#import telnetlib
-#import time
-#import traceback
-#import datetime
-#import uuid
+
+import psycopg2
+import dbconstants
+import datetime
+import uuid
 from lib.connection_utils import SSHConnection, TelnetConnection
-#from threading import Thread
+
+def update_db(out, execution_params):
+
+    conn=psycopg2.connect(**dbconstants.DBCONNECTION_PARAMS)
+    sql_update_log = """INSERT INTO public.netauto_log("Run_ID","Netauto_Module","IP_Hostname","OEM","Executed_Date","Executed_By","Run_log","Connection_Type")
+             		   VALUES(%s,%s,%s,%s,%s,%s,%s,%s);"""
+    ip_hostname = ','.join(execution_params['deviceAddresses'])
+    unique_uuid=uuid.uuid4().hex
+    Update_EachIP_log = (unique_uuid,
+                         "Remote Command Execution",
+                         ip_hostname,
+                         execution_params['OEM'],
+                         datetime.datetime.now(),
+                         "Admin",
+                         out,
+                         execution_params['deviceConnectionType'])
+    cur=conn.cursor()
+    cur.execute(sql_update_log, Update_EachIP_log)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return None
 
 def remote_execution(execution_params):
     if execution_params['deviceConnectionType'] == 'telnet':
@@ -41,7 +57,9 @@ def remote_execution(execution_params):
         c = SSHConnection(execution_params)
     else:
         raise Exception("Invalid connection type")
-    return c.execute()
+    out = c.execute()
+    update_db(out, execution_params)
+    return out
 
 def test_all(execution_params):
     print("Case-1: SSH with Jumpbox\n")
@@ -65,20 +83,17 @@ def test_all(execution_params):
 
 if __name__=="__main__":
     in1={
-        "jmpServerIp":"192.168.0.30",
-        "jmpServerUsername":"admin",
-        "jmpServerPassword":"admin",
+        "jmpServerIp":"34.207.61.121",
+        "jmpServerUsername":"ubuntu",
+        "jmpServerPassword":"ubuntu",
         "OEM":"cisco",
         "deviceUsername":"admin",
         "devicePassword":"admin",
-        "deviceAddresses":["10.1.1.20","10.1.1.21"],
+        "deviceAddresses":["172.31.61.171"],
         "commands":["show version", "show ip int br"],
         "deviceConnectionType":"ssh",
-        "isJumpServer":True
+        "isJumpserver":True
         }
-    out = test_all(in1)
-    #out=remote_execution(in1)
+    #out = test_all(in1)
+    out=remote_execution(in1)
     print(out)
-
-
-
